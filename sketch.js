3dcac14223fc4a6e0ed78da01fce894233682306
sharjigs <52230631+sharjigs@users.noreply.github.com>
2020-07-04 20:23:47 +0530

    Updates

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let img;
 function setup() {
 	createCanvas(640, 480);
 	video = createCapture(VIDEO);
-	img = loadImage('https://www.pngarts.com/files/4/Male-Face-PNG-Image-Transparent.png'); 
+	// img = loadImage('https://www.pngarts.com/files/4/Male-Face-PNG-Image-Transparent.png'); 
 	video.hide();
 	poseNet = ml5.poseNet(video, modelLoaded);
 	poseNet.on('pose', gotPoses);
@@ -25,8 +25,8 @@ function modelLoaded() {
 function draw() {
 	image(video, 0, 0);
 	if(pose) {
-	// fill(255, 0, 0);
-	// ellipse(pose.nose.x, pose.nose.y, 64);
-	image(img, pose.nose.x, pose.nose.y);
+	fill(255, 0, 0);
+	ellipse(pose.nose.x, pose.nose.y, 64);
+	//image(img, pose.nose.x, pose.nose.y);
 	}
 }
