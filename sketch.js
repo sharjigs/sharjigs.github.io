75fda18163277da1d3546d9fe5a4ca6dd24c7270
sharjigs <52230631+sharjigs@users.noreply.github.com>
2020-07-04 20:22:03 +0530

    Update with New image

--- sketch.js
+++ sketch.js
@@ -1,18 +1,15 @@
 let video;
 let poseNet;
 let pose;
-
+let img;
 function setup() {
 	createCanvas(640, 480);
 	video = createCapture(VIDEO);
+	img = loadImage('https://www.pngarts.com/files/4/Male-Face-PNG-Image-Transparent.png'); 
 	video.hide();
 	poseNet = ml5.poseNet(video, modelLoaded);
 	poseNet.on('pose', gotPoses);
 }
-
-function preload() { 
-  img = loadImage('sample-image.png'); 
-} 
 
 function gotPoses(poses) {
 	console.log(poses);
@@ -28,7 +25,8 @@ function modelLoaded() {
 function draw() {
 	image(video, 0, 0);
 	if(pose) {
-	fill(255, 0, 0);
-	ellipse(pose.nose.x, pose.nose.y, 64);
+	// fill(255, 0, 0);
+	// ellipse(pose.nose.x, pose.nose.y, 64);
+	image(img, pose.nose.x, pose.nose.y);
 	}
 }
